b4e3a1cc7c67a95882b50cf741f2e6168a66ddf3
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2022-09-22 19:21:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# exercism-cli
+
+Integrate the Exercism CLI with VS Code
+
+## Features
+
+- Install the CLI
+- Configure the CLI
+- Pick / start a track
+- Upgrade the CLI
+
+## Requirements
+
+Your system must be supported by the CLI.
+
+- If the Exercism CLI is already installed, it's version must be >= 3, < 4.
+- If you change the workspace location, make sure that it still has `Exercism` in its name.
+
+## Extension Settings
+
+This extension doesn't (yet) add any settings.
+
+## Known Issues
+
+None at the moment.
+
+## Release Notes
+
+### 0.1.0
+
+Initial release of the extension
---
+++ src/execute.ts
@@ -0,0 +1,27 @@
+import { exec as childProcessExec } from "child_process";
+
+export function exec(
+  cmd: string,
+  logError = console.error,
+  logVerbose = console.debug
+) {
+  logVerbose(`[exec] ${cmd}`);
+
+  return new Promise<string>((resolve, reject) => {
+    childProcessExec(cmd, (err, out) => {
+      if (err) {
+        logError(`[exec] error while running ${cmd}`);
+        logError(err);
+
+        return reject(err);
+      }
+      logVerbose(`[exec] out for ${cmd}`, out);
+
+      return resolve(out);
+    });
+  });
+}
+
+export function execSafe(cmd: string) {
+  return exec(cmd, console.debug).catch((error) => error.message);
+}
